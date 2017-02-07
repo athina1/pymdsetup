5bd6d4c0cf0f7b5291f7ae22b6f18a1230ee8d52
Pau Andrio <andriopau@gmail.com>
2017-02-07 17:17:39 +0100

    longer tests

--- run_pycompss_mn.sh
+++ run_pycompss_mn.sh
@@ -3,7 +3,7 @@
 enqueue_compss \
   --exec_time=$1 \
   --num_nodes=$2 \
-  --queue=debug \
+  --queue=bsc_ls \
   --tasks_per_node=16 \
   --master_working_dir=. \
   --worker_working_dir=gpfs \
@@ -12,7 +12,7 @@ enqueue_compss \
   --pythonpath=/gpfs/home/bsc51/bsc51210/pymdsetup/:/gpfs/home/bsc51/bsc51210/ \
   --tracing=$3 \
   --graph=$4 \
-  --debug \
+  --log_level=off \
 /gpfs/home/bsc51/bsc51210/pymdsetup/workflows/gromacs_full_pycompss_test.py $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
 
-#./run_pycompss_mn.sh 10 3 false false
+#./run_pycompss_mn.sh 120 3 false false
--- workflows/gromacs_full_pycompss_test.py
+++ workflows/gromacs_full_pycompss_test.py
@@ -309,14 +309,20 @@ def main():
                      output_xvg_path=p_rmsd.xvg,
                      log_path=p_rmsd.out, error_path=p_rmsd.err, gmx_path=gmx_path)
 
+        rmsd_xvg_path_dict[mut] = p_rmsd.xvg
+
+    waitForAllTasks()
     print ('step18: gnuplot ----- Creating RMSD plot')
     p_gnuplot = conf.step_prop('step18_gnuplot', workflow_path)
-    gnuplotPyCOMPSs(dependency_file_in=opj(p_rmsd.path, 'step17_rmsd.task'),
-                    dependency_file_out=opj(p_gnuplot.path, 'step18_gnuplot.task'),
+    gnuplotPyCOMPSs(dependency_file_out=opj(p_gnuplot.path, 'step18_gnuplot.task'),
                     task_path=p_gnuplot.path,
-                    input_xvg_path_dict=p_gio.gro,
-                    log_path=p_rmsd.out, error_path=p_rmsd.err, gnuplot_path=gnuplot_path)
-
+                    input_xvg_path_dict=rmsd_xvg_path_dict,
+                    output_png_path=p_gnuplot.png,
+                    output_plotscript_path=p_gnuplot.plotscript,
+                    log_path=p_gnuplot.out, error_path=p_gnuplot.err, gnuplot_path=gnuplot_path)
+    png=compss_wait_on(opj(p_gnuplot.path, 'step18_gnuplot.task'))
+    elapsed_time = time.time() - start_time
+    print "Elapsed time: ", elapsed_time
 
 ############################## PyCOMPSs functions #############################
 @task(dependency_file_in=FILE_IN, dependency_file_out=FILE_OUT, task_path=IN,
@@ -540,17 +546,24 @@ def rmsdPyCOMPSs(dependency_file_in, dependency_file_out, task_path,
     open(dependency_file_out, 'a').close()
 
 
-@task(dependency_file_in=FILE_IN, dependency_file_out=FILE_OUT, task_path=IN,
+@task(dependency_file_out=FILE_OUT,
+      task_path=IN,
       input_xvg_path_dict=IN,
+      output_png_path=IN,
+      output_plotscript_path=IN,
       log_path=IN, error_path=IN, gnuplot_path=IN)
-def gnuplotPyCOMPSs(dependency_file_in, dependency_file_out, task_path,
+def gnuplotPyCOMPSs(dependency_file_out,
+                    task_path,
                     input_xvg_path_dict,
+                    output_png_path,
+                    output_plotscript_path,
                     log_path, error_path, gnuplot_path):
     fu.create_change_dir(task_path)
     gnuplot.Gnuplot46(input_xvg_path_dict=input_xvg_path_dict,
+                      output_png_path=output_png_path,
+                      output_plotscript_path=output_plotscript_path,
                       log_path=log_path, error_path=error_path, gnuplot_path=gnuplot_path).launch()
     open(dependency_file_out, 'a').close()
-
 ##############################################################################
 
 
