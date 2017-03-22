578416510ab960f9b5af7bb94bfbe6493c0b11a7
User2 <andriopau@gmail.com>
2017-03-22 17:50:47 +0100

    Adding mug_refinement

--- scwrl_wrapper/scwrl.py
+++ scwrl_wrapper/scwrl.py
@@ -29,9 +29,11 @@ class Scwrl4(object):
                  log_path=None, error_path=None, scwrl_path=None):
         self.input_pdb_path = input_pdb_path
         self.output_pdb_path = output_pdb_path
-        pattern = re.compile(("(?P<chain>[a-zA-Z]{1}).(?P<wt>[a-zA-Z]{3})"
-                              "(?P<resnum>\d+)(?P<mt>[a-zA-Z]{3})"))
-        self.mutation = pattern.match(mutation).groupdict()
+        self.mutation = mutation
+        if self.mutation is not None:
+            pattern = re.compile(("(?P<chain>[a-zA-Z]{1}).(?P<wt>[a-zA-Z]{3})"
+                                  "(?P<resnum>\d+)(?P<mt>[a-zA-Z]{3})"))
+            self.mutation = pattern.match(mutation).groupdict()
         self.log_path = log_path
         self.error_path = error_path
         self.scwrl_path = scwrl_path
@@ -39,36 +41,39 @@ class Scwrl4(object):
     def launch(self):
         """Launches the execution of the SCWRL binary.
         """
-        # Read structure with Biopython
-        parser = PDBParser(PERMISSIVE=1)
-        st = parser.get_structure('s', self.input_pdb_path)  # s random id never used
+        if self.mutation is not None:
+            # Read structure with Biopython
+            parser = PDBParser(PERMISSIVE=1)
+            st = parser.get_structure('s', self.input_pdb_path)  # s random id never used
 
-        # Remove the side chain of the AA to be mutated
-        chain = self.mutation['chain']
-        resnum = int(self.mutation['resnum'])
-        residue = st[0][chain][(' ', resnum, ' ')]
-        backbone_atoms = ['N', 'CA', 'C', 'O', 'CB']
-        not_backbone_atoms = []
+            # Remove the side chain of the AA to be mutated
+            chain = self.mutation['chain']
+            resnum = int(self.mutation['resnum'])
+            residue = st[0][chain][(' ', resnum, ' ')]
+            backbone_atoms = ['N', 'CA', 'C', 'O', 'CB']
+            not_backbone_atoms = []
 
-        # The following formula does not work. Biopython bug?
-        # for atom in residue:
-        #     if atom.id not in backbone_atoms:
-        #         residue.detach_child(atom.id)
+            # The following formula does not work. Biopython bug?
+            # for atom in residue:
+            #     if atom.id not in backbone_atoms:
+            #         residue.detach_child(atom.id)
 
-        for atom in residue:
-            if atom.id not in backbone_atoms:
-                not_backbone_atoms.append(atom)
-        for atom in not_backbone_atoms:
-            residue.detach_child(atom.id)
+            for atom in residue:
+                if atom.id not in backbone_atoms:
+                    not_backbone_atoms.append(atom)
+            for atom in not_backbone_atoms:
+                residue.detach_child(atom.id)
 
-        # Change residue name
-        residue.resname = self.mutation['mt'].upper()
+            # Change residue name
+            residue.resname = self.mutation['mt'].upper()
 
-        # Write resultant structure
-        w = PDBIO()
-        w.set_structure(st)
-        prepared_file_path = self.output_pdb_path + '.scwrl4.prepared.pdb'
-        w.save(prepared_file_path)
+            # Write resultant structure
+            w = PDBIO()
+            w.set_structure(st)
+            prepared_file_path = self.output_pdb_path + '.scwrl4.prepared.pdb'
+            w.save(prepared_file_path)
+        else:
+            prepared_file_path = self.input_pdb_path
 
         scrwl = 'Scwrl4' if self.scwrl_path is None else self.scwrl_path
         cmd = [scrwl, '-i', prepared_file_path, '-o', self.output_pdb_path]
