4281358f7611c85ac42d78c9ff217c756d1dd69a
andrio <pau.andrio@bsc.es>
2017-03-22 18:16:00 +0100

    mug_refi

--- workflows/gromacs_full.py
+++ workflows/gromacs_full.py
@@ -41,7 +41,7 @@ except ImportError:
 
 def main():
     start_time = time.time()
-    sys_paths = 'macos'
+    sys_paths = 'pycompss_open_nebula'
     root_dir = os.path.dirname(os.path.abspath(sys.modules[__name__].__file__))
     conf_file_path = os.path.join(root_dir, 'conf_test.yaml')
     conf = settings.YamlReader(yaml_path=(conf_file_path))
