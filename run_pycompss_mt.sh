86499d33d19079e00fc8cd8e62e3fe69f43b875e
Pau Andrio <andriopau@gmail.com>
2017-10-24 15:00:05 +0200

    run_pycompss_mt debug off

--- run_pycompss_mt.sh
+++ run_pycompss_mt.sh
@@ -17,7 +17,7 @@ enqueue_compss \
   --worker_working_dir=/gpfs/scratch/bsc23/bsc23210/ \
   --tracing=$4 \
   --graph=$5 \
-  --log_level=debug \
+  --log_level=off \
 /gpfs/home/bsc23/bsc23210/pymdsetup/workflows/gromacs_full_pycompss.py $6 $7 $3 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
 
 #/gpfs/home/bsc23/bsc23210/pymdsetup/run_pycompss_mt.sh None 20 3 false false /gpfs/home/bsc23/bsc23210/pymdsetup/workflows/conf_2mut_gpu_test.yaml minotauro
