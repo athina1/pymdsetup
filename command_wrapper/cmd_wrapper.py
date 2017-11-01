f7c64b62e52e6ab47d633d1676544ecb70044c5d
Pau Andrio <andriopau@gmail.com>
2017-11-01 13:43:27 +0100

    cmd_wrapper deleting unwanted print

--- command_wrapper/cmd_wrapper.py
+++ command_wrapper/cmd_wrapper.py
@@ -20,13 +20,10 @@ class CmdWrapper(object):
         if self.out_log is None:
             print ''
             print "cmd_wrapper commnand print: " + cmd
-        print "cmd_wrapper commnand print: " + cmd
         new_env = os.environ.copy()
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE, shell=True,
                                    env=new_env)
-        # process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-        #                            stderr=subprocess.PIPE, shell=True)
 
         out, err = process.communicate()
         if self.out_log is None:
