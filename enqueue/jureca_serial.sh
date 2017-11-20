06f4691b9904f992abbf38c512d80483dc390939
Pau Andrio <andriopau@gmail.com>
2017-11-20 14:49:22 +0100

    Adding python path to jureca_serial

--- enqueue/jureca_serial.sh
+++ enqueue/jureca_serial.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python workflows/gromacs_full.py workflows/conf/conf_2mut_nt0_1ps.yaml jureca 1
+export PYTHONPATH=/homea/ias-5/andrio/pymdsetup:$PYTHONPATH
+/homea/ias-5/andrio/anaconda2/bin/python2.7 workflows/gromacs_full.py workflows/conf/conf_2mut_nt0_1ps.yaml jureca 1
