b9dcd0b27570fc307ce035d68405dc3b0f099d58
Pau Andrio <andriopau@gmail.com>
2018-03-13 13:08:00 +0100

    Adding DUDE api to provide decoys for docking

--- ebi_api/pdb.py
+++ ebi_api/pdb.py
@@ -9,31 +9,23 @@ class EbiPdb(object):
 
     This class is used to download PDB files from the european PDB repository
     (http://www.ebi.ac.uk/pdbe)
-
-    Args:
-        pdb_code (str): Protein Data Bank (PDB) four letter code.
-            ie: '2ki5'
-        output_pdb_path (str): File path where the PDB file will be stored.
-            ie: '/home/user1/2ki5.pdb'
     """
 
-    def __init__(self, pdb_code, output_pdb_path):
-        self._pdb_code = pdb_code
-        self._output_pdb_path = output_pdb_path
+    def __init__(self, url=None):
+        self.url = url if url else "http://www.ebi.ac.uk/pdbe/entry-files/"
 
-    def get_pdb(self):
+    def get_pdb(self, pdb_code, output_pdb_path):
         """
         Writes the PDB file content of `self._pdb_code`
         to `self._output_pdb_path`
         """
-        if os.path.isfile(self._pdb_code):
-            shutil.copy(self._pdb_code, self._output_pdb_path)
+        if os.path.isfile(pdb_code):
+            shutil.copy(pdb_code, output_pdb_path)
         else:
-            url = ("http://www.ebi.ac.uk/pdbe/entry-files/"
-                   "pdb"+self._pdb_code.lower()+".ent")
+            url = (self.url+"pdb"+pdb_code.lower()+".ent")
             pdb_string = requests.get(url).content
             lines = pdb_string.splitlines(True)
 
-            with open(self._output_pdb_path, 'w') as pdb_file:
+            with open(output_pdb_path, 'w') as pdb_file:
                 pdb_file.write(''.join(itertools.dropwhile(
                                lambda line: line[:6] != "ATOM  ", lines)))
